4d909f45f77958754a89b5c00c529b5827d0e47a
Habeeb Mohammad <69910360+habsion@users.noreply.github.com>
2021-08-16 00:06:44 +0530

    Update Rock-Paper-scissor.py

--- Rock-Paper-scissor.py
+++ Rock-Paper-scissor.py
@@ -7,11 +7,12 @@ user_name=input('Player name: ')
 print('Enter input as rock/paper/scissor or stop to terminate the game')
 while True:                                # run the loop until the player gives input as "stop"
     user=input('user: ').lower()
+    if user=='stop':
+        print('Game terminated!')
+        break
     if user not in t:                      # if the user gives an input outside of sample space then stop the current iteration and move to the next iteration
         print('Invalid input!!!!')
         continue
-    if user=='stop':
-        break
     com=randint(0,2)                       # generate a random int from 0 to 2 which acts as a index and that index gives our bot it's pick from the sample space
     print('Computer:',t[com])
     
